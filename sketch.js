06cab3cdb0fd76579a03f4dc2bb804310b47582d
haldun <haldunbucak97@outlook.com>
2024-03-06 14:30:16 +0100

    Added functions for curve determination and connection removal

--- sketch.js
+++ sketch.js
@@ -62,23 +62,47 @@ function draw() {
 		draggedModule.drag();
 	}
 
-	for (let i=0; i<moduleList.length; i++) {
-		moduleList[i].draw(font);
-	}
-
 	// Draw the Connections
 	push();
-	strokeWeight(8);
+	strokeWeight(6);
+	fill(40, 0);
 	for (let i = 0; i<connections.length; i++) {
+		// Check pins for determining curve direction
+		let controlDst = 500;
+		let firstDir = 1;
+		let secondDir = 1;
+		if (connections[i][0].isInput) {
+			firstDir = -1;
+		}
+		if (connections[i][1].isInput) {
+			secondDir = -1;
+		}
 		stroke(connections[i][0].state*128, 0, 0);
-		line(connections[i][0].x, connections[i][0].y, connections[i][1].x, connections[i][1].y);
+		curve(connections[i][0].x - firstDir*controlDst, connections[i][0].y, 
+				  connections[i][0].x, connections[i][0].y, 
+				  connections[i][1].x, connections[i][1].y, 
+				  connections[i][1].x - secondDir*controlDst, connections[i][1].y);
 	}
 
 	if (clickedPin != null) {
+		// Check pins for determining curve direction
+		let controlDst = 500;
+		let Dir = 1;
+		if (clickedPin.isInput) {
+			Dir = -1;
+		}
 		stroke(clickedPin.state*128, 0, 0);
-		line(clickedPin.x, clickedPin.y, mouseX, mouseY);
+		curve(clickedPin.x - Dir*controlDst, clickedPin.y, 
+					clickedPin.x, clickedPin.y,
+					mouseX, mouseY,
+					mouseX + Dir*controlDst, mouseY);
 	}
 	pop();
+
+	// Draw the Modules
+	for (let i=0; i<moduleList.length; i++) {
+		moduleList[i].draw(font);
+	}
 }
 
 function mousePressed() {
@@ -112,7 +136,14 @@ function mouseClicked() {
 						clickedPin = moduleList[i].inputs[j];
 					}
 					else if (clickedPin != moduleList[i].inputs[j]){
-						connections.push([clickedPin, moduleList[i].inputs[j]]);
+						// If the chosen connection is not already established, do it
+						if (!listContainsList(connections, [clickedPin, moduleList[i].inputs[j]])) {
+							connections.push([clickedPin, moduleList[i].inputs[j]]);
+						}
+						// If it is already established, remove it
+						else {
+							connections.splice(indexOfList(connections, [clickedPin, moduleList[i].inputs[j]]), 1);
+						}
 						clickedPin = null;
 					}
 				}
@@ -123,11 +154,50 @@ function mouseClicked() {
 						clickedPin = moduleList[i].outputs[j];
 					}
 					else if (clickedPin != moduleList[i].outputs[j]){
-						connections.push([clickedPin, moduleList[i].outputs[j]]);
+						// If the chosen connection is not already established, do it
+						if (!listContainsList(connections, [moduleList[i].outputs[j], clickedPin])) {
+							connections.push([moduleList[i].outputs[j], clickedPin]);
+						}
+						// If it is already established, remove it
+						else {
+							connections.splice(indexOfList(connections, [moduleList[i].outputs[j], clickedPin]), 1);
+						}
 						clickedPin = null;
 					}
 				}
 			}
 		}
 	}
+}
+
+// Just Like "IndexOf" but designed to work for lists within lists
+function indexOfList(searchList, targetList) {
+	for (let i = 0; i < searchList.length; i++) {
+		if (listsEqual(searchList[i], targetList)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+function listContainsList(searchList, targetList) {
+	for (let i = 0; i < searchList.length; i++) {
+		if (listsEqual(searchList[i], targetList)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// Code by enyo from Stack Overflow
+function listsEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
